27a9ab331753ffe74da92075e278ecbd598c9de3
Kate <ms.katesy@gmail.com>
2018-07-04 15:08:53 +1000

    put all winning patterns logic into one function, added logic to reset game board after 1 round

--- app.js
+++ app.js
@@ -2,81 +2,108 @@ var board = document.querySelector(".board");
 var rows = document.querySelectorAll(".row");
 var boxes = document.querySelectorAll(".row div");
 var turnCount = 0;
+var winnerOutput = document.querySelector(".winner");
+var winnerFound = false;
 
 var checkRow = function(boxClass){
   rows.forEach(function(row){
-    var marksToWin = 0;
+    var numMarksInLine = 0;
     for (var x = 0; x < rows.length; x++){
       if (row.children[x].classList.contains(boxClass) === true){
-        marksToWin += 1;
+        numMarksInLine += 1;
       }
-      if (marksToWin === rows.length){
-      console.log(boxClass);  
-      return
+      if (numMarksInLine === rows.length){
+        winnerFound = true;
+        winnerOutput.textContent = boxClass + " wins!";  
       }
     }
   })
-}
+} 
 
 var checkCol = function(boxClass){
   for (var x = 0; x < rows.length; x++){
-    var marksToWin = 0;
+    var numMarksInLine = 0;
     rows.forEach(function(row){
       if (row.children[x].classList.contains(boxClass) === true){
-        marksToWin += 1;
+        numMarksInLine += 1;
       }
-      if (marksToWin === rows.length){
-        console.log(boxClass); 
+      if (numMarksInLine === rows.length){
+        winnerFound = true;
+        winnerOutput.textContent = boxClass; 
       }
     })
   }
 }
 
 var checkRightDiagonal = function(boxClass){
-  var marksToWin = 0;
+  var numMarksInLine = 0;
   for (var i = 0; i < rows.length; i++){
     if (rows[i].children[i].classList.contains(boxClass) === true){
-      marksToWin += 1;
+      numMarksInLine += 1;
     }
-    if (marksToWin === rows.length){
-      console.log(boxClass); 
+    if (numMarksInLine === rows.length){
+      winnerFound = true;
+      winnerOutput.textContent = boxClass; 
     }
   }
 }
 
 var checkLeftDiagonal = function(boxClass){
-  var marksToWin = 0;
+  var numMarksInLine = 0;
   for (var x = 0; x < rows.length; x++){
       if (rows[x].children[rows.length-1-x].classList.contains(boxClass) === true){
-        marksToWin += 1;
+        numMarksInLine += 1;
       }
-      if (marksToWin === rows.length){
-        console.log(boxClass); 
+      if (numMarksInLine === rows.length){
+        winnerFound = true;
+        winnerOutput.textContent = boxClass;
       }
   }
 }
 
+var checkForWinner = function(boxClass){
+    checkRow(boxClass);
+    checkCol(boxClass);
+    checkRightDiagonal(boxClass);
+    checkLeftDiagonal(boxClass);
+}
+
 var markBox = function(event){
+
+
   if (event.target.classList != 0){
     return
   }
   else{ 
-    if (turnCount % 2 === 0){
+    if (turnCount % 2 === 0){ 
       event.target.classList.add("player1");
-      checkRow("player1");
-      checkCol("player1");
-      checkRightDiagonal("player1");
-      checkLeftDiagonal("player1");
+      checkForWinner("player1");
     }
     else {
       event.target.classList.add("player2")
-      checkRow("player2");
-      checkCol("player2");
-      checkRightDiagonal("player2");
-      checkLeftDiagonal("player2");
+      checkForWinner("player2");
     }  
     turnCount += 1
   }
+
+  if (turnCount === 8 && winnerFound === false){
+    winnerOutput.textContent = "Draw";
+    boxes.forEach(function(box){
+      box.className = "";
+    })
+    turnCount = 0;
+  }
+
+  else if (winnerFound === true){
+    boxes.forEach(function(box){
+      box.className = "";
+    })
+    turnCount = 0;
+    winnerFound = false;
+    // winnerOutput.textContent = "";
+  }
+
+  
 }
 
 
